6a0a778db70df919e9be03671f035afaf33e4b06
kuomay <guomei88119@gmail.com>
2023-11-24 14:27:08 +0800

    feat navbar切版

--- pages/index.vue
+++ pages/index.vue
@@ -5,5 +5,6 @@
     <div>
         <NuxtLink to="/login">login</NuxtLink>
         <NuxtLink to="/sign-up">sign up</NuxtLink>
+        <NuxtLink to="/main-page"> | main page</NuxtLink>
     </div>
 </template>
