35d8ba0a7d404d6783c3e1c8865e37d6b7f4fe2c
CY-Ho <agho@geosat.com.tw>
2023-11-24 09:18:09 +0800

    modified: login page style

--- pages/login.vue
+++ pages/login.vue
@@ -7,19 +7,17 @@ const visible = ref(false)
 </script>
 
 <template>
-    <div class="sign-box">
-        <v-img
-            class="mx-auto my-3"
-            max-width="228"
-            src="~/assets/images/logo-showed.png"
-        ></v-img>
-
+    <div class="sign-container">
         <v-card
-            class="mx-auto pa-4 pb-4"
+            class="pa-4 pb-2"
             elevation="8"
-            max-width="30%"
             rounded="lg"
         >
+            <v-img
+                src="~/assets/images/faked-logo.png"
+                width="8rem"
+                class="mx-auto"></v-img>
+
             <div class="text-subtitle-1 text-medium-emphasis">Login with...</div>
             <button class="sign-btn"><img src="~/assets/images/google.png"></button>
             <button class="sign-btn"><img src="~/assets/images/facebook.png"></button>
@@ -37,10 +35,10 @@ const visible = ref(false)
                 Password
 
                 <a
-                class="text-caption text-decoration-none text-blue"
-                href="#"
-                rel="noopener noreferrer"
-                target="_blank"
+                    class="text-caption text-decoration-none text-blue"
+                    href="#"
+                    rel="noopener noreferrer"
+                    target="_blank"
                 >
                 Forgot login password?</a>
             </div>
@@ -56,7 +54,7 @@ const visible = ref(false)
 
             <v-btn
                 block
-                class="mb-4"
+                class="mb-1"
                 color="blue"
                 size="large"
                 variant="tonal"
@@ -64,14 +62,22 @@ const visible = ref(false)
                 Log In
             </v-btn>
 
-            <v-card-text class="text-center">
+            <v-card-text class="d-flex justify-space-between">
                 <a
-                class="text-blue text-decoration-none"
-                href="#"
-                rel="noopener noreferrer"
-                target="_blank"
+                    class="text-blue text-decoration-none"
+                    href="#"
+                    rel="noopener noreferrer"
+                    target="_blank"
                 >
                 Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
+                </a>
+                <a
+                    class="text-blue text-decoration-none"
+                    href="#"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                >
+                Change Language
                 </a>
             </v-card-text>
         </v-card>
