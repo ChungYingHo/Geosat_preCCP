b36106140c34f6f3f9ea0e8fa494afabb3e57beb
CY-Ho <agho@geosat.com.tw>
2023-11-27 17:05:40 +0800

    feat: add footer

--- stores/headerStore.ts
+++ stores/headerStore.ts
@@ -1,21 +1,26 @@
 import { defineStore } from 'pinia';
 
-export const useHeaderStore = defineStore({
-  id: 'header', // 唯一标识符
-  state: () => ({
-    // 这里定义你的状态
-  }),
-  actions: {
-    // 在这里定义你的逻辑
-    getLink(item: string): string {
-      switch (item) {
-        case 'Home':
-          return '/main-page';
-        case 'UAV':
-          return '/uav';
-        default:
-          return '/main-page';
+export const useHeaderStore = defineStore('header', ()=>{
+    const getLink = (item: string): string =>{
+        switch (item) {
+          case 'Home':
+            return '/main-page';
+          case 'UAV':
+            return '/uav';
+          default:
+            return '/main-page';
+        }
       }
-    },
-  },
+
+    const icons = [
+      'mdi-facebook',
+      'mdi-twitter',
+      'mdi-linkedin',
+      'mdi-instagram',
+    ]
+
+    return {
+      getLink,
+      icons
+    }
 });
